a16bb548556902b8a0f12b5dd4dbee09e403a4fc
yahiro <yahiro07@users.noreply.github.com>
2026-06-04 11:43:29 +0900

    add field name guard

--- src/store.ts
+++ src/store.ts
@@ -6,6 +6,11 @@ import { ChangesListener, Mutations, Store } from "./types";
 const immer = new Immer({ autoFreeze: false });
 
 export function createStore<T extends object>(initialState: T): Store<T> {
+  if ("state" in initialState) {
+    throw new Error(
+      "The store state object cannot have a field name with 'state'.",
+    );
+  }
   type K = Extract<keyof T, string>;
   type V = T[K];
 
